648e112da526679fa194f552ccb86be76dbb3de9
Teslasoft <odima1403@gmail.com>
2021-06-02 12:14:15 +0300

    v1.16.xx
    See changelog for more info

--- client-side/app/src/main/java/com/teslasoft/id/smartcard/NfcAuthenticator.java
+++ client-side/app/src/main/java/com/teslasoft/id/smartcard/NfcAuthenticator.java
@@ -1,132 +1,122 @@
 package com.teslasoft.id.smartcard;
 
-import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.net.Uri;
+import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
-import android.content.Intent;
-import android.widget.Toast;
-import android.os.Parcelable;
-import android.widget.TextView;
-import android.app.PendingIntent;
-import java.nio.charset.Charset;
-import java.util.Arrays;
-import android.provider.Settings;
-import android.util.Log;
-import java.util.List;
-import android.content.IntentFilter;
-import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
-import android.content.ComponentName;
-import com.google.gson.Gson;
-import java.util.Map;
-import java.util.HashMap;
-import android.net.Uri;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
-import java.util.List;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.Arrays;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.pm.Signature;
-import java.security.MessageDigest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
-public class NfcAuthenticator extends Activity{
+public class NfcAuthenticator extends Activity {
 	public TextView nfc_message;
 	public NfcAdapter nfcAdapter;
 	public PendingIntent pendingIntent;
-    IntentFilter writeTagFilters[];
+	public IntentFilter[] writeTagFilters;
     boolean writeMode;
-    Tag myTag;
-    Context context;
-	String url;
+	public Tag myTag;
+	public Context context;
+	public String url;
 	public LinearLayout loadbar;
 	public LinearLayout actions;
-	public int RESPONSE_CODE;
-	private int SELF_SIGNATURE = 0;
-	private int SELF_DEBUG_SIGNATURE = 0;
-	private int DEFAULT_SIGNATURE = -672009692;
-	private int GOOGLE_APPS_SIGNATURE = -473270056;
-	private int ANDROID_SIGNATURE = -810989147;
-	private String appSignature;
 	private String signatureHash;
-	private String isNotification;
 	public Signature sig;
-	
+	public int versionCode = BuildConfig.VERSION_CODE;
+	public String versionName = BuildConfig.VERSION_NAME;
+
+	/***********************************************************
+	 * Unimplemented:
+	 * public int RESPONSE_CODE;
+	 * private int SELF_SIGNATURE = 0;
+	 * private int SELF_DEBUG_SIGNATURE = 0;
+	 * private int DEFAULT_SIGNATURE = -672009692;
+	 * private int GOOGLE_APPS_SIGNATURE = -473270056;
+	 * private int ANDROID_SIGNATURE = -810989147;
+	 * private String appSignature;
+	 * private String isNotification;
+	 **********************************************************/
+
+	@SuppressLint("PackageManagerGetSignatures")
 	@Override
-	protected void onCreate(Bundle savedInstanceState){
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+		overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
 		setContentView(R.layout.nfc_read);
-		nfc_message=(TextView) findViewById(R.id.nfc_message);
-		loadbar=(LinearLayout) findViewById(R.id.loadbar);
-		actions=(LinearLayout) findViewById(R.id.actions);
-		context=this;
-		try{
-		sig = this.getPackageManager().getPackageInfo(this.getPackageName(), PackageManager.GET_SIGNATURES).signatures[0];
-		signatureHash = Integer.toString(sig.hashCode());
-		//toast("Debug: " + signatureHash, this);
-		}catch (Exception e) {
+		nfc_message = findViewById(R.id.nfc_message);
+		loadbar = findViewById(R.id.loadbar);
+		actions = findViewById(R.id.actions);
+		context = this;
+
+		try {
+			sig = this.getPackageManager().getPackageInfo(this.getPackageName(), PackageManager.GET_SIGNATURES).signatures[0];
+			signatureHash = Integer.toString(sig.hashCode());
+			// toast("Debug: " + signatureHash, this);
+		} catch(Exception e) {
 			new AlertDialog.Builder(this)
 				.setTitle("Verification failed")
-				.setMessage("We can not check licence because we can not check application signature. [ERR_APP_INVALID_SIGNATURE_HASH]: -2")
+				.setMessage("We can not check license because we can not check application signature. [ERR_APP_INVALID_SIGNATURE_HASH]: -2")
 				.setCancelable(false)
-				// Specifying a listener allows you to take an action before dismissing the dialog.
-				// The dialog is automatically dismissed when a dialog button is clicked.
-				.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) { 
-						finishAndRemoveTask();
-					}
-				})
-
-				// A null listener allows the button to dismiss the dialog and take no further action.
+				.setPositiveButton(android.R.string.ok, (dialog, which) -> finishAndRemoveTask())
 				.show();
 		}
-		try{Intent intent = new Intent(Intent.ACTION_MAIN);
-		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		intent.setComponent(new ComponentName("com.teslasoft.libraries.support","com.teslasoft.jarvis.core.SmartCardLoader"));
-		intent.addCategory(Intent.CATEGORY_LAUNCHER);
-		startActivity(intent);}
-		catch(Exception eee){}
-		String android_id = android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
-		if (verifyInstallerId(this) == false) {
-		//toast(android_id, this);
-			if (android_id.equals("1c38f735d4d8ea04")) {
-			toast("WARNING! A test device detected. Licence check skipped. Current signature hash: " + signatureHash + " ,device ID: " + android_id,this);
-		} else {
-		new AlertDialog.Builder(this)
-			.setTitle("Verification failed")
-			.setMessage("We can not check licence because this app installed from third-party source. Try to install it from Google Play. We perform this check to prevent tampering with API and security attacks. [ERR_PREFERAL_INSTALLED_BY_PACKAGE_INSTALLER]: -1")
-			.setCancelable(false)
-			// Specifying a listener allows you to take an action before dismissing the dialog.
-			// The dialog is automatically dismissed when a dialog button is clicked.
-			.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) { 
-					finishAndRemoveTask();
-				}
-			})
 
-			// A null listener allows the button to dismiss the dialog and take no further action.
-			.show();
-		}}
+		try {
+			Intent intent = new Intent(Intent.ACTION_MAIN);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			intent.setComponent(new ComponentName("com.teslasoft.libraries.support", "com.teslasoft.jarvis.core.SmartCardLoader"));
+			intent.addCategory(Intent.CATEGORY_LAUNCHER);
+			startActivity(intent);
+		}
+
+		catch(Exception ignored) {}
+
+		@SuppressLint("HardwareIds") String android_id = android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+		if (!verifyInstallerId(this)) {
+			// DEBUG: toast(android_id, this);
+			if (android_id.equals("1c38f735d4d8ea04") || android_id.equals("6f612b6eb1950998")) {
+				toast("WARNING! A test device detected. License check skipped. Current signature hash: " + signatureHash + ", device ID: " + android_id,this);
+			} else {
+				new AlertDialog.Builder(this)
+					.setTitle("Verification failed")
+					.setMessage("We can not check license because this app installed from third-party source. Try to install it from Google Play. We perform this check to prevent tampering with API and security attacks. [ERR_PREFERAL_INSTALLED_BY_PACKAGE_INSTALLER]: -1")
+					.setCancelable(false)
+					.setPositiveButton(android.R.string.ok, (dialog, which) -> finishAndRemoveTask())
+					.show();
+			}
+		}
 		nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 		if(nfcAdapter == null){
-			toast("This device doesn't support NFC.",this);
+			toast("This device doesn't support NFC.", this);
 			finishAndRemoveTask();
 		}
-			readFromIntent(getIntent());
-			pendingIntent=PendingIntent.getActivity(this,0,new Intent(this,getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
-			IntentFilter tagDetected=new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
-			tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
-			writeTagFilters=new IntentFilter[]{tagDetected};
+
+		readFromIntent(getIntent());
+		pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+		IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+		tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+		writeTagFilters = new IntentFilter[] { tagDetected };
 	}
 
 	/* Piracy check starts */
@@ -142,117 +132,122 @@ public class NfcAuthenticator extends Activity{
 	}
 	/* Piracy check ends */
 	
-	private void readFromIntent(Intent intent){
+	private void readFromIntent(Intent intent) {
         String action=intent.getAction();
-        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
-			|| NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
-			|| NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)){
-            Parcelable[] rawMsgs=intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-			byte[] tagId=intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);	
-			String hexdump=new String();
-			for(int i=0;i<tagId.length;i++){
-				String x=Integer.toHexString(((int) tagId[i] & 0xff));
-				if (x.length()==1){x='0'+x;}
-				hexdump+=x;
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action) || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action) || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+			byte[] tagId = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+			StringBuilder hexdump = new StringBuilder();
+			for (byte b : tagId) {
+				String x = Integer.toHexString(((int) b & 0xff));
+				if (x.length() == 1) {
+					x = '0' + x;
+				}
+				hexdump.append(x);
 			}
-            NdefMessage[] msgs=null;
-            if (rawMsgs!=null){
-                msgs=new NdefMessage[rawMsgs.length];
-                for(int i=0;i<rawMsgs.length;i++) msgs[i]=(NdefMessage) rawMsgs[i];
+
+            NdefMessage[] msgs = null;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for(int i = 0; i < rawMsgs.length; i++) msgs[i] = (NdefMessage) rawMsgs[i];
             }
-            buildTagViews(msgs,hexdump);
+            buildTagViews(msgs, hexdump.toString());
         }
 	}
+
 	private void buildTagViews(NdefMessage[] msgs, String tagId) {
-        if (msgs==null||msgs.length==0) return;
-        String text="";
-		byte[] payload=msgs[0].getRecords()[0].getPayload();
-        String textEncoding=((payload[0]&128)==0) ? "UTF-8":"UTF-16";
-        int languageCodeLength=payload[0]&0063;
-        try{text=new String(payload,languageCodeLength+1,payload.length-languageCodeLength-1,textEncoding);
-        }catch(Exception e){Log.e("UnsupportedEncoding",e.toString());}
-		Authenticate(text,tagId);
+        if (msgs == null || msgs.length == 0) return;
+        String text = "";
+		byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+        int languageCodeLength = payload[0] & 51;
+        try {
+        	text = new String(payload, languageCodeLength+1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch(Exception e) {
+        	Log.e("UnsupportedEncoding", e.toString());
+        }
+
+		Authenticate(text, tagId);
     }
+
 	@Override
-    protected void onNewIntent(Intent intent){
+    protected void onNewIntent(Intent intent) {
         setIntent(intent);
         readFromIntent(intent);
-        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
-            myTag=intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
         }
    	}
+
 	@Override
-    public void onPause(){
+    public void onPause() {
         super.onPause();
-        writeMode=false;
+        writeMode = false;
         nfcAdapter.disableForegroundDispatch(this);
     }
+
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
-        writeMode=true;
-        nfcAdapter.enableForegroundDispatch(this,pendingIntent,writeTagFilters,null);
+        writeMode = true;
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, writeTagFilters, null);
     }
-	public void Authenticate(String token,String serial){
-		try{
-			Gson gson=new Gson();
-			Map<String,Object> map=new HashMap<String,Object>();
-			map=(Map<String,Object>) gson.fromJson(token,map.getClass());
-			String user_fwid=map.get("fwid").toString();
-			String user_id=map.get("uid").toString();
-			String pincode_encrypted=map.get("pin").toString();
-			token=user_fwid.trim();
-			token=token.toUpperCase();
+
+	public void Authenticate(String token, String serial) {
+		try {
+			Gson gson = new Gson();
+			Map <String,Object> map = new HashMap<>();
+			map = (Map<String,Object>) gson.fromJson(token,map.getClass());
+			String user_fwid = Objects.requireNonNull(map.get("fwid")).toString();
+			String user_id = Objects.requireNonNull(map.get("uid")).toString();
+			String pincode_encrypted = Objects.requireNonNull(map.get("pin")).toString();
+			token = user_fwid.trim();
+			token = token.toUpperCase();
 			nfc_message.setText("SmartCard Info\n\nSerial Number: ".concat(serial.concat("\nSmartCard ID: ".concat(user_fwid.concat("\nUser ID: ".concat(user_id))))));
-			url="https://id.teslasoft.org/smartcard/auth?cbs=".concat(token).concat("&sid=").concat(serial).concat("&pwtoken=").concat(pincode_encrypted).concat("&uid=").concat(user_id);
+			url = "https://id.teslasoft.org/smartcard/auth?cbs=".concat(token).concat("&sid=").concat(serial).concat("&pwtoken=").concat(pincode_encrypted).concat("&uid=").concat(user_id).concat("&ver=").concat(Integer.valueOf(versionCode).toString()).concat("&vn=").concat(versionName);
 			loadbar.setVisibility(View.GONE);
 			actions.setVisibility(View.VISIBLE);
-		}catch(Exception e){
+		} catch(Exception e) {
 			new AlertDialog.Builder(this)
 				.setTitle("Error")
-				.setMessage("An error occured while reading data from the SmartCard. Please make sure that you used valid SmartCard.")
+				.setMessage("An error occurred while reading data from the SmartCard. Please make sure that you used valid SmartCard.")
 				.setCancelable(false)
-				// Specifying a listener allows you to take an action before dismissing the dialog.
-				// The dialog is automatically dismissed when a dialog button is clicked.
-				.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) { 
-						finishAndRemoveTask();
-					}
-				})
-
-				// A null listener allows the button to dismiss the dialog and take no further action.
+				.setPositiveButton(android.R.string.ok, (dialog, which) -> finishAndRemoveTask())
 				.show();
 		}
 	}
-	public void Auth(View v){openUrl(url);}
-	public void Close(View v){finishAndRemoveTask();}
-	public void openUrl(String url){
+
+	public void Auth(View v) {
+		openUrl(url);
+	}
+
+	public void Close(View v) {
+		finishAndRemoveTask();
+	}
+
+	public void openUrl(String url) {
 		final String urls=url;
-		final Handler handler=new Handler();
-		handler.postDelayed(new Runnable(){
-			@Override
-			public void run(){
-				overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
-				Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse(urls));
-				startActivity(browserIntent);
-				final Handler handler=new Handler();
-				handler.postDelayed(new Runnable(){
-					@Override
-					public void run(){
-						overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
-						finishAndRemoveTask();
-					}
-				}, 10);
-			}
+		final Handler handler = new Handler();
+		handler.postDelayed(() -> {
+			overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+			Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(urls));
+			startActivity(browserIntent);
+			final Handler handler1 = new Handler();
+			handler1.postDelayed(() -> {
+				overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+				finishAndRemoveTask();
+			}, 10);
 		}, 1000);
 	}
 	
 	public Handler mHandler = new Handler();
-	public void toast(final CharSequence text,final Context context){
-        mHandler.post(new Runnable(){
-			@Override public void run(){
-				try{Toast.makeText(context,text,Toast.LENGTH_SHORT).show();}
-				catch(Exception e){}
+	public void toast(final CharSequence text, final Context context) {
+        mHandler.post(() -> {
+			try {
+				Toast.makeText(context,text,Toast.LENGTH_SHORT).show();
+			}
+			catch(Exception ignored) {
+
 			}
 		});
     }
